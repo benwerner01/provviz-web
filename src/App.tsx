2def8fe4839ac5e8f7952b6015e4545d49d154ef
Ben Werner <ben.werner01@gmail.com>
2020-12-01 15:45:48 +0100

    Visualiser

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Visualiser } from 'provviz';
+import examples from './lib/examples';
+
+type Dimensions = {
+  width: number;
+  height: number;
+}
 
 function App() {
+  const [
+    visualiserDimensions,
+    setVisualiserDimensions] = useState<Dimensions | undefined>();
+
+  const calculateDimensions = () => {
+    setVisualiserDimensions({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  };
+
+  useEffect(() => {
+    calculateDimensions();
+    window.addEventListener('resize', calculateDimensions);
+    return () => {
+      window.removeEventListener('resize', calculateDimensions);
+    };
+  }, []);
+
   return (
-    <div className="App">Hello World</div>
+    <div className="App">
+      {visualiserDimensions && (
+        <Visualiser
+          document={examples[0].document}
+          wasmFolderURL={`${process.env.PUBLIC_URL}wasm`}
+          width={visualiserDimensions.width}
+          height={visualiserDimensions.height}
+        />
+      )}
+    </div>
   );
 }
 
