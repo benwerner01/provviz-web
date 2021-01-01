c9a59a41735431035ba00d4dbca9cf0d2c374066
Ben Werner <ben.werner01@gmail.com>
2021-01-01 19:50:03 +0000

    MenuBar component

---
+++ src/components/MenuBar.tsx
@@ -0,0 +1,101 @@
+import React, { useRef, useState } from 'react';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+import Popper from '@material-ui/core/Popper';
+import Button from '@material-ui/core/Button';
+import Menu, { MenuItem, SubMenu } from 'rc-menu';
+import 'rc-menu/assets/index.css';
+import { ClickAwayListener } from '@material-ui/core';
+import examples from '../lib/examples';
+
+export const MENU_BAR_HEIGHT = 36;
+
+const useStyles = makeStyles((theme) => ({
+  wrapper: {
+    height: MENU_BAR_HEIGHT,
+    padding: theme.spacing(0, 2),
+    backgroundColor: theme.palette.primary.dark,
+  },
+  logo: {
+    margin: theme.spacing(0, 2, 0, 0),
+    fontSize: 24,
+    fontWeight: 800,
+    color: theme.palette.common.white,
+  },
+  button: {
+    borderRadius: 0,
+    fontWeight: 800,
+    color: theme.palette.common.white,
+    '&:hover': {
+      backgroundColor: theme.palette.primary.main,
+    },
+  },
+  menuPopper: {
+    width: 300,
+  },
+  menu: {
+    marginTop: 0,
+    padding: 0,
+    backgroundColor: theme.palette.common.white,
+  },
+  subMenu: {
+    '&:hover': {
+      cursor: 'pointer',
+    },
+  },
+  menuItem: {
+    '&:hover': {
+      cursor: 'pointer',
+    },
+  },
+}));
+
+type MenuBarProps = {
+  setDocument: (document: object) => void;
+}
+
+const MenuBar: React.FC<MenuBarProps> = ({ setDocument }) => {
+  const classes = useStyles();
+  const openButton = useRef<HTMLButtonElement>(null);
+
+  const [openMenuIsOpen, setOpenMenuIsOpen] = useState<boolean>(false);
+
+  return (
+    <Box display="flex" alignItems="center" className={classes.wrapper}>
+      <h1 className={classes.logo}>ProvViz</h1>
+      <Button
+        className={classes.button}
+        onClick={() => setOpenMenuIsOpen(!openMenuIsOpen)}
+        ref={openButton}
+      >
+        Open
+      </Button>
+      <Popper
+        open={openMenuIsOpen}
+        className={classes.menuPopper}
+        placement="bottom-start"
+        anchorEl={openButton.current}
+      >
+        <ClickAwayListener onClickAway={() => setOpenMenuIsOpen(false)}>
+          <Menu className={classes.menu}>
+            <SubMenu className={classes.subMenu} title="Examples">
+              {examples.map(({ name, document }) => (
+                <MenuItem
+                  className={classes.menuItem}
+                  onClick={() => setDocument(document)}
+                >
+                  {name}
+                </MenuItem>
+              ))}
+            </SubMenu>
+            <MenuItem>
+              Upload
+            </MenuItem>
+          </Menu>
+        </ClickAwayListener>
+      </Popper>
+    </Box>
+  );
+};
+
+export default MenuBar;
