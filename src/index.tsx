a706baea62024fe764bbd8ef0ccc182a1683c9ef
Ben Werner <ben.werner01@gmail.com>
2021-01-01 15:18:47 +0000

    prevent mui className clashes

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import App from './App';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'provviz-web', seed: 'provviz-web',
+});
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StylesProvider generateClassName={generateClassName}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </StylesProvider>,
   document.getElementById('root'),
 );
